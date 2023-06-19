0bc76077402474e7f63652cdeacc5f52f5c47dce
Andrew Roberts <sombreroman55@gmail.com>
2023-06-18 21:38:20 -0600

    Each tab now has its own settings

--- palette-helper.py
+++ palette-helper.py
@@ -1,8 +1,8 @@
 from PIL import Image
-from collections import Counter
 import os
 import math
 import sys
+import utils
 from PyQt6.QtCore import Qt, QTimer, pyqtSignal
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import \
@@ -10,35 +10,43 @@ from PyQt6.QtWidgets import \
     QHBoxLayout, QVBoxLayout, QPushButton, QFileDialog, QGridLayout, \
     QSlider
 
+# TODO: Add UI to assign colors to a scheme. Start with kitty themes
+#       Since they are an easy 16 color palette that  we can work with
+#       We can expand to more if we want to for emacs or vim or whatever
+
 
 class ColorSquare(QLabel):
     def __init__(self, rgba_hex):
         super().__init__()
-        self.rgba = rgba_hex
-        self.negative = tuple(~component & 0xFF for component in rgba_hex)
+        self.rgba = utils.col2hex(rgba_hex)
+        self.negative = utils.col2hex(
+                tuple(~component & 0xFF for component in rgba_hex))
         self.timer = QTimer()
-        self.isCopying = False
+        self.is_copying = False
+        self.defineStyles()
         self.initUI()
+
+    def defineStyles(self):
+        common = f"background-color: {self.rgba}; color: {self.negative};"
+        self.styles = {
+            "normal": common,
+            "hover": common + f" border: 2px solid {self.negative};",
+            "copying": "background-color: green; color: white;",
+        }
 
     def initUI(self):
         self.setMouseTracking(True)
-        self.style = f"background-color : {self.col2hex(self.rgba)}; color : {self.col2hex(self.negative)};"
-        self.hover_style = self.style + f"border: 2px solid {self.col2hex(self.negative)};"
-        self.copied_style = self.style + "background-color: green; color: white;"
-        self.setText(f"{self.col2hex(self.rgba)}")
+        self.setText(f"{self.rgba}")
         self.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.setStyleSheet(self.style)
-
-    def col2hex(self, colorTuple):
-        return '#{:02x}{:02x}{:02x}'.format(*colorTuple)
+        self.setStyleSheet(self.styles["normal"])
 
     def enterEvent(self, event):
-        if not self.isCopying:
-            self.setStyleSheet(self.hover_style)
+        if not self.is_copying:
+            self.setStyleSheet(self.styles["hover"])
 
     def leaveEvent(self, event):
-        if not self.isCopying:
-            self.setStyleSheet(self.style)
+        if not self.is_copying:
+            self.setStyleSheet(self.styles["normal"])
 
     def mousePressEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
@@ -48,117 +56,34 @@ class ColorSquare(QLabel):
             event.accept()
 
     def showCopied(self):
-        self.isCopying = True
+        self.is_copying = True
         self.setText("Copied!")
-        self.setStyleSheet(self.copied_style)
+        self.setStyleSheet(self.styles["copying"])
         self.timer.stop()
         self.timer.setInterval(400)
         self.timer.setSingleShot(True)
-        self.timer.timeout.connect(self.restoreWidget)
+        self.timer.timeout.connect(self.restore)
         self.timer.start()
 
-    def restoreWidget(self):
-        self.isCopying = False
-        self.setText(f"{self.col2hex(self.rgba)}")
-        self.setStyleSheet(self.style)
-
-
-class PaletteTabs(QTabWidget):
-    tabChanged = pyqtSignal(str)
-
-    def __init__(self):
-        super().__init__()
-        self.images = None
-        self.min_distance = 25
-        self.num_colors = 16
-        self.colors = []
-        self.initUI()
-
-    def initUI(self):
-        self.currentChanged.connect(self.changeTab)
-
-    def changeTab(self, index):
-        imagePath = self.tabText(index)
-        self.tabChanged.emit(imagePath)
-
-    def addImages(self, images):
-        self.images = images
-        self.analyze()
-
-    def updateMinDistance(self, distance):
-        self.min_distance = distance
-        self.analyze()
-
-    def updateNumColors(self, colors):
-        self.num_colors = colors
-        self.analyze()
-
-    def colorDistance(self, color1, color2):
-        # Discard alpha channel
-        r1, g1, b1, _ = color1
-        r2, g2, b2, _ = color2
-        return math.sqrt((r2 - r1) ** 2 + (g2 - g1) ** 2 + (b2 - b1) ** 2)
-
-    def analyze(self):
-        self.clear()
-        for image in self.images:
-            colors = self.getMostFrequentColors(image)
-            newTab = QWidget()
-            layout = QGridLayout()
-            rows = math.ceil(math.sqrt(self.num_colors))
-            for i, color in enumerate(colors):
-                square = ColorSquare(color[0][:3])
-                layout.addWidget(square, i // rows, i % rows)
-            newTab.setLayout(layout)
-            self.colors.append(colors)
-            self.addTab(newTab, os.path.basename(image))
-
-    def getMostFrequentColors(self, image):
-        image = Image.open(image)
-        image = image.convert('RGBA')
-        pixels = list(image.getdata())
-        color_count = Counter(pixels)
-        sorted_colors = color_count.most_common()
-
-        most_frequent_colors = []
-        selected_colors = []
-
-        for color, count in sorted_colors:
-            if color[3] <= 200:
-                continue
-            is_similar = False
-            for selected_color in selected_colors:
-                if self.colorDistance(color, selected_color) <= self.min_distance:
-                    is_similar = True
-                    break
-
-            if not is_similar:
-                most_frequent_colors.append((color, count))
-                selected_colors.append(color)
-
-            if len(most_frequent_colors) == self.num_colors:
-                break
-
-        return most_frequent_colors
+    def restore(self):
+        self.is_copying = False
+        self.setText(f"{self.rgba}")
+        self.setStyleSheet(self.styles["normal"])
 
 
 class PaletteParameters(QWidget):
-    files_selected = pyqtSignal(list)
     distance_changed = pyqtSignal(int)
     colors_changed = pyqtSignal(int)
 
-    def __init__(self):
+    def __init__(self, image):
         super().__init__()
+        self.image_path = image
         self.num_colors = 16
         self.min_distance = 25
         self.initUI()
 
     def initUI(self):
         layout = QVBoxLayout()
-
-        self.file_button = QPushButton("Open Files")
-        self.file_button.clicked.connect(self.selectFiles)
-
         self.color_label = QLabel(f"Colors: {self.num_colors}")
         self.color_slider = QSlider(Qt.Orientation.Horizontal)
         self.color_slider.setRange(1, 40)
@@ -173,10 +98,8 @@ class PaletteParameters(QWidget):
 
         self.image_label = QLabel("Original image")
         self.original_image = QLabel()
-        self.image_label.hide()
-        self.original_image.hide()
+        self.original_image.setPixmap(QPixmap(self.image_path))
 
-        layout.addWidget(self.file_button)
         layout.addWidget(self.color_label)
         layout.addWidget(self.color_slider)
         layout.addWidget(self.distance_label)
@@ -184,7 +107,6 @@ class PaletteParameters(QWidget):
         layout.addWidget(self.image_label)
         layout.addWidget(self.original_image)
 
-        layout.setStretchFactor(self.file_button, 1)
         layout.setStretchFactor(self.color_label, 1)
         layout.setStretchFactor(self.color_slider, 1)
         layout.setStretchFactor(self.distance_label, 1)
@@ -202,18 +124,93 @@ class PaletteParameters(QWidget):
         self.distance_label.setText(f"Min distance: {value}")
         self.distance_changed.emit(value)
 
-    def showOriginal(self, image_path):
-        image = QPixmap(image_path)
-        self.original_image.setPixmap(image)
-        self.image_label.show()
-        self.original_image.show()
 
-    def selectFiles(self):
-        file_dialog = QFileDialog()
-        file_dialog.setFileMode(QFileDialog.FileMode.ExistingFiles)
-        if file_dialog.exec() == QFileDialog.DialogCode.Accepted:
-            selected_files = file_dialog.selectedFiles()
-            self.files_selected.emit(selected_files)
+class PaletteTabView(QWidget):
+    def __init__(self, image_path):
+        super().__init__()
+        self.image_path = image_path
+        self.min_distance = 25
+        self.num_colors = 16
+        self.alpha_cutoff = 200
+        self.extractAllColors()
+        self.initUI()
+
+    def extractAllColors(self):
+        image = Image.open(self.image_path)
+        image = image.convert('RGBA')
+        self.colors = list(image.getdata())
+
+    def getColors(self):
+        return self.colors
+
+    def updateMinDistance(self, distance):
+        self.min_distance = distance
+        self.updateGrid()
+
+    def updateNumColors(self, colors):
+        self.num_colors = colors
+        self.updateGrid()
+
+    def initUI(self):
+        layout = QHBoxLayout()
+        self.parameters = PaletteParameters(self.image_path)
+        self.grid_view = QWidget()
+        grid_layout = QGridLayout()
+        self.grid_view.setLayout(grid_layout)
+
+        layout.addWidget(self.parameters)
+        layout.addWidget(self.grid_view)
+        layout.setStretchFactor(self.parameters, 1)
+        layout.setStretchFactor(self.grid_view, 3)
+
+        self.parameters.colors_changed.connect(self.updateNumColors)
+        self.parameters.distance_changed.connect(self.updateMinDistance)
+        self.updateGrid()
+        self.setLayout(layout)
+
+    def updateGrid(self):
+        layout = self.grid_view.layout()
+        for i in reversed(range(layout.count())):
+            layout.itemAt(i).widget().setParent(None)
+        most_used = utils.mostCommonColors(self.colors,
+                                           self.num_colors,
+                                           self.min_distance,
+                                           self.alpha_cutoff)
+        rows = math.ceil(math.sqrt(self.num_colors))
+        for i, color in enumerate(most_used):
+            square = ColorSquare(color[:3])
+            layout.addWidget(square, i // rows, i % rows)
+
+
+class PaletteTabs(QTabWidget):
+    def __init__(self):
+        super().__init__()
+        self.images = set()
+        self.open_tabs = set()
+        self.initUI()
+
+    def initUI(self):
+        self.setTabsClosable(True)
+        self.tabCloseRequested.connect(self.closeTab)
+
+    def closeTab(self, index):
+        widget = self.widget(index)
+        self.removeTab(index)
+        widget.deleteLater()
+
+    def addImages(self, images):
+        # N^2 but who cares
+        for image in images:
+            already_here = False
+            for tab in range(self.count()):
+                widget = self.widget(tab)
+                if image == widget.image_path:
+                    already_here = True
+                    break
+
+            if not already_here:
+                newTabView = PaletteTabView(image)
+                self.addTab(newTabView, os.path.basename(image))
 
 
 class PaletteAnalyzer(QMainWindow):
@@ -226,23 +223,22 @@ class PaletteAnalyzer(QMainWindow):
         self.setGeometry(100, 100, 1280, 720)
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
-        layout = QHBoxLayout(central_widget)
-        self.parameters = PaletteParameters()
+        layout = QVBoxLayout(central_widget)
+        self.file_button = QPushButton("Open Files")
+        self.file_button.clicked.connect(self.selectFiles)
         self.tabs = PaletteTabs()
-        layout.addWidget(self.parameters)
+        layout.addWidget(self.file_button)
         layout.addWidget(self.tabs)
-
-        layout.setStretchFactor(self.parameters, 1)
-        layout.setStretchFactor(self.tabs, 3)
-        self.parameters.files_selected.connect(self.tabs.addImages)
-        self.parameters.colors_changed.connect(self.tabs.updateNumColors)
-        self.parameters.distance_changed.connect(self.tabs.updateMinDistance)
-        self.tabs.tabChanged.connect(self.parameters.showOriginal)
+        layout.setStretchFactor(self.file_button, 1)
+        layout.setStretchFactor(self.tabs, 8)
         self.show()
 
-    # TODO: Add UI to assign colors to a scheme. Start with kitty themes
-    #       Since they are an easy 16 color palette that  we can work with
-    #       We can expand to more if we want to for emacs or vim or whatever
+    def selectFiles(self):
+        file_dialog = QFileDialog()
+        file_dialog.setFileMode(QFileDialog.FileMode.ExistingFiles)
+        if file_dialog.exec() == QFileDialog.DialogCode.Accepted:
+            selected_files = file_dialog.selectedFiles()
+            self.tabs.addImages(selected_files)
 
 
 if __name__ == "__main__":
